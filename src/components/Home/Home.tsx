74442c80449db2e4eda13cf4f3a2416c16e72554
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-22 22:30:18 -0700

    revise

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -7,7 +7,6 @@ import HowPsycheScreenWork from './HowPsycheScreenWork';
 import AI from './AI';
 import Footer from './Footer';
 import HealthCare from './HealthCare';
-import { Outlet } from 'react-router-dom';
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,7 +25,6 @@ export default function Home() {
       {/* Patient Screening Modal */}
       <Footer />
       {isModalOpen && <PatinentScreenModal setIsModalOpen={setIsModalOpen} />}
-      <Outlet />
     </div>
   );
 }
